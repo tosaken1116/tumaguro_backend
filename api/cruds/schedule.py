964382895adbdd298445feaec0f3dc94af5d43cf
tosaken <v-ntosa@alterbooth.net>
2023-05-17 00:50:23 +0900

    add:schedule更新エンドポイントの追加

--- api/cruds/schedule.py
+++ api/cruds/schedule.py
@@ -1,6 +1,7 @@
+from fastapi import HTTPException
+
 from api.db.models import Schedule
 from api.schema.schedule import Schedule as ScheduleSchema
-from fastapi import HTTPException
 
 
 def add_new_schedule(db,name,start_time,end_time,user_id):
@@ -25,3 +26,15 @@ def delete_schedule_by_id(db,schedule_id,user_id):
         return {"status":"success"}
     except:
         raise HTTPException(status_code=500,detail="delete failed")
+
+def update_schedule_by_id(db,schedule_id,user_id,name,start_time,end_time):
+    update_schedule_orm = db.query(Schedule).filter(Schedule.id == schedule_id).filter(Schedule.user_id == user_id).first()
+    if update_schedule_orm is None:
+        raise HTTPException(status_code=404,detail="schedule does not exist")
+    update_schedule_orm.name = name
+    update_schedule_orm.start_time = start_time
+    update_schedule_orm.end_time = end_time
+    db.add(update_schedule_orm)
+    db.commit()
+    db.refresh(update_schedule_orm)
+    return ScheduleSchema.from_orm(update_schedule_orm)
--- api/routers/schedule.py
+++ api/routers/schedule.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm.session import Session
 
-from api.cruds.schedule import add_new_schedule, get_schedule_by_user_id,delete_schedule_by_id
+from api.cruds.schedule import (add_new_schedule, delete_schedule_by_id,
+                                get_schedule_by_user_id, update_schedule_by_id)
 from api.db.database import get_db
-from api.schema.schedule import GetScheduleResult, PostSchedule, Schedule,DeleteScheduleResult
+from api.schema.schedule import (DeleteScheduleResult, GetScheduleResult,
+                                 PostSchedule, Schedule)
 from api.utils.auth import get_current_user
 
 schedule_router = APIRouter()
@@ -15,5 +17,8 @@ async def add_schedule(payload:PostSchedule,db:Session=Depends(get_db),current_u
 async def add_schedule(db:Session=Depends(get_db),current_user=Depends(get_current_user)):
     return get_schedule_by_user_id(db,current_user.id)
 @schedule_router.delete("/{schedule_id}",response_model=DeleteScheduleResult)
-async def delete_schedule(schedule_id:str,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+async def delete_schedule_one(schedule_id:str,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
     return delete_schedule_by_id(db,schedule_id,current_user.id)
+@schedule_router.put("/{schedule_id}",response_model=Schedule)
+async def update_schedule_one(schedule_id:str,payload:PostSchedule,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+    return update_schedule_by_id(db,schedule_id,current_user.id,payload.name,payload.start_time,payload.end_time)
