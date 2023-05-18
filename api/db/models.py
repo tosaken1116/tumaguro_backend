2c99e9e5ed6c9a864c3d1ffc2b7a88639f4da5cf
tosaken <v-ntosa@alterbooth.net>
2023-05-18 17:49:22 +0900

    fix:idが衝突するエラーの解消

--- api/db/models.py
+++ api/db/models.py
@@ -1,21 +1,22 @@
-import uuid
 from datetime import datetime
+from uuid import uuid4
 
 from sqlalchemy import Boolean, Column, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
+def gen_uuid():
+    return str(uuid4())
 class User(Base):
     __tablename__ = 'user'
-    id = Column(String,primary_key=True,default=str(uuid.uuid4()))
+    id = Column(String,primary_key=True,default=gen_uuid)
     email = Column(String,unique=True,nullable=False)
     username = Column(String,nullable=False)
     password_hash = Column(String,nullable=False)
 
 class Task(Base):
     __tablename__ = 'task'
-    id = Column(String,primary_key=True,default=str(uuid.uuid4()))
+    id = Column(String,primary_key=True,default=gen_uuid)
     name = Column(String,nullable=False)
     created_at = Column(DateTime,default=datetime.now().strftime('%x %X'))
     finished_at = Column(DateTime,nullable=True,default=None)
@@ -24,7 +25,7 @@ class Task(Base):
 
 class Schedule(Base):
     __tablename__ = 'schedule'
-    id = Column(String,primary_key=True,default=str(uuid.uuid4()))
+    id = Column(String,primary_key=True,default=gen_uuid)
     name = Column(String,nullable=False)
     created_at = Column(DateTime,default=datetime.now().strftime('%x %X'))
     start_time = Column(DateTime,nullable=False)
